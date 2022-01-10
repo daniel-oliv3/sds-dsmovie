3dc68ef624d6fe015c5881f8328daf21fda225f3
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-10 14:50:54 -0300

    Backend criado

---
+++ backend/src/test/java/com/sapup3/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapup3.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
