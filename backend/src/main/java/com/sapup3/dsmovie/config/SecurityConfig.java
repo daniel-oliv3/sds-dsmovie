7cddb78ae687df1bb50bdb858477db4165a59350
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-12 15:53:02 -0300

    Domain model, database seed

--- backend/src/main/java/com/sapup3/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/sapup3/dsmovie/config/SecurityConfig.java
@@ -1,5 +1,19 @@
 package com.sapup3.dsmovie.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
