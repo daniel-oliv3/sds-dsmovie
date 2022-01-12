5cabad50085cb78f12fc0c4f5a8ffbde6b90330f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-12 11:18:54 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,7 +6,25 @@ function Listing(){
         <>
             <Pagination />
 
-            <MovieCard />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
